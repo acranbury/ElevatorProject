a44427f9b1538ba2969aca9856845c179dfc6b19
noudyk <noudyk-cc@conestogac.on.ca>
2014-05-29 21:17:50 -0400

    Fixed up spi files to remove errors
    -Added in derivative.h include for registers
    -Commented out an incorrect and duplicate register call
    - Commented out function code that included undefined macros (not
    available even in utils.h)

--- CodeWarrior/Sources/spi.c
+++ CodeWarrior/Sources/spi.c
@@ -1,3 +1,4 @@
+#include "derivative.h"
 #include "spi.h"
 #define SPICR1_INIT 0b01010000
 #define SPI1MBAUD	0b00000010
@@ -5,7 +6,7 @@
 // initialize SPI module
 void SPIinit(void){
 	// baud rate divisor for 1Mbps with 8 MHz E-clock
-	SPIBD = 0x02;
+	//SPIBD = 0x02; should be SPIBR = 0x02, which is already done later
 
 	//configure SPI port for:
 	// SPI on, no intr, master, CPOL-0, CPHA=0, MSB 1st
@@ -15,12 +16,14 @@ void SPIinit(void){
 
 // put a command to the SPI module
 void SPIcmd(char cmd, char data){
-	CLR_BITS(SPICR1, SPICR1_SSOE_MASK);
+/* undefined utility macros
+  CLR_BITS(SPICR1, SPICR1_SSOE_MASK);
 	
 	SPIputc(LO_NYBBLE(cmd) | HI_NYBBLE(data));
 	SPIputc(LO_NYBBLE(data));
 	
 	SET_BITS(SPICR1, SPICR1_SSOE_MASK);
+*/
 	
 }
 
