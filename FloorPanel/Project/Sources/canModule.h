71c8b6a01fa8c4343d6aba2a0b2e708389197ea3
Alan Cranbury <alancranbury@gmail.com>
2014-06-04 11:55:16 -0400

    Ready for Debrief

---
+++ FloorPanel/Project/Sources/canModule.h
@@ -0,0 +1,68 @@
+#ifndef __CANMODULE__
+#define __CANMODULE__
+
+#include "derivative.h"      /* derivative-specific definitions */
+
+#define NO_ERR 1
+#define CAN_MSG_LENGTH 8
+
+#define MASK_CODE(id) ((~(id)) >> 16)       
+#define ACC_CODE(id) (((id)&0xFFFF0000) >> 16)
+
+/* Acceptance Code Definitions */
+#define ACC_CODE_ID100 0x2000   // Hex 0x100 message ID
+#define ACC_CODE_ID100_HIGH ((ACC_CODE_ID100 & 0xFF00) >> 8)
+#define ACC_CODE_ID100_LOW (ACC_CODE_ID100 & 0x00FF)
+
+/* Mask Code Definitions */
+#define MASK_CODE_ST_ID 0x0007
+#define MASK_CODE_ST_ID_HIGH ((MASK_CODE_ST_ID & 0xFF00) >> 8)
+#define MASK_CODE_ST_ID_LOW (MASK_CODE_ST_ID & 0xFF)
+
+/*
+#define MASK_CODE_CONTROLLER_ID 0xFFDF
+#define MASK_CODE_ELEVATOR_CAR_ID 0xFFBF
+#define MASK_CODE_FLOOR_1_ID 0xFF7F
+#define MASK_CODE_FLOOR_2_ID 0xFEFF
+#define MASK_CODE_FLOOR_3_ID 0xFDFF
+*/
+
+//Node ID Definitions
+#define ST_ID_100 0x20000000    //test id
+
+#define CONTROLLER_ID (0x00010000 << 5)   //shifting by 5 bits to fit in 11 bit CAN address
+#define ELEVATOR_CAR_ID (0x00020000 << 5)
+#define FLOOR_1_ID (0x00040000 << 5)
+#define FLOOR_2_ID (0x00080000 << 5)
+#define FLOOR_3_ID (0x00100000 << 5)
+#define BROADCAST_ID (0xFFE00000)
+
+//Messsage IDs
+#define ELEV_LOCATION 0x00
+#define CALL_BTN_PRESS 0x01
+#define PANEL_BTN_PRESS 0x02
+#define APPEND_DISPLAY_CHAR 0x03
+#define ERROR_MSG 0xFF
+
+// Message Contents
+#define FLOOR_NUM_1	0x01
+#define FLOOR_NUM_2 0x02
+#define FLOOR_NUM_3 0x03
+#define CALL_BTN_UP 0x01
+#define CALL_BTN_DN 0x02
+
+typedef struct {
+	unsigned long id;
+	unsigned char priority;
+	unsigned char length;
+	unsigned char *payload;
+} CANMessage;
+
+extern unsigned char canRXData[8];
+extern char canRXFlag;
+
+
+void initCan (void);
+void sendCanFrame (CANMessage message);
+
+#endif
---
+++ FloorPanel/Project/Sources/main.c
@@ -0,0 +1,196 @@
+#include <hidef.h>      /* common defines and macros */
+#include <string.h>
+#include "derivative.h"      /* derivative-specific definitions */
+#include "spi.h"
+#include "timer.h"
+#include "canModule.h"
+
+#define BUTF3UP	0b10000000
+#define BUTF3DN	0b01000000
+#define BUTF2UP	0b00100000
+#define BUTF2DN	0b00010000
+#define BUTF1UP	0b00000100
+#define BUTF1DN	0b00000001
+
+#define LEDF3UP 0b00000010
+#define LEDF3DN	0b00001000
+#define LEDF2UP	0b00100000
+#define LEDF2DN	0b10000000
+#define LEDF1UP	0b00010000
+#define LEDF1DN	0b01000000
+
+#define NUM0 0b01111101
+#define NUM1 0b00000101
+#define NUM2 0b00111110
+#define NUM3 0b01111010
+#define NUM4 0b01100011
+#define NUM5 0b01011011
+#define NUM6 0b01011111
+#define NUM7 0b01110000
+#define NUM8 0b01111111
+#define NUM9 0b01110011
+#define NUMA 0b01110111
+#define NUMB 0b01001111
+#define NUMC 0b00011101
+#define NUMD 0b01101110
+#define NUME 0b00011111
+#define NUMF 0b00010111
+
+void main(void) {
+
+	CANMessage txMessage;
+	unsigned char txBuffer[CAN_MSG_LENGTH];
+	unsigned char rxBuffer[CAN_MSG_LENGTH];
+	int i;
+	
+	txMessage.id = BROADCAST_ID;
+	txMessage.priority = 0;
+	txMessage.length = CAN_MSG_LENGTH;
+	txMessage.payload = txBuffer;
+	
+	timer_init();
+	
+	SPIinit();
+	
+	initCan();
+	
+	while (!(CANCTL0 & 0x10));
+
+	CANRFLG = 0xC3;
+	CANRIER = 0x01;
+	
+	for(i = 0; i < 8; i++)
+	{
+		txBuffer[i] = 0;
+		rxBuffer[i] = 0;
+	}
+	
+	EnableInterrupts;
+	
+	DDRA = 0xFF;
+	DDRB = 0x00;
+	//DDRP = 0x00;
+	
+	//PTP = 0xff;
+	SPIputc(0x00);
+
+
+	
+	while(1)
+	{
+		if(canRXFlag)
+		{
+			// grab the command from the buffer
+		//	DisableInterrupts;
+			for(i=0; i<8; i++){
+			    rxBuffer[i]=canRXData[i];
+			}
+		//	EnableInterrupts;
+			
+			if(rxBuffer[0] == 0x00)
+			{
+				switch(rxBuffer[1])
+				{
+					case FLOOR_NUM_1:
+						SPIputc(NUM1);
+						switch(rxBuffer[2])
+						{/*
+							case CALL_BTN_UP:
+								CLR_BITS(PORTA,LEDF1UP); 
+								break;
+							case CALL_BTN_DN:
+								CLR_BITS(PORTA,LEDF1DN);
+								break;*/
+							default:
+								CLR_BITS(PORTA,LEDF1UP|LEDF1DN);
+								break;
+						}
+						break;
+					case FLOOR_NUM_2:
+						SPIputc(NUM2);
+						switch(rxBuffer[2])
+						{/*
+							case CALL_BTN_UP:
+								CLR_BITS(PORTA,LEDF2UP);
+								break;
+							case CALL_BTN_DN:
+								CLR_BITS(PORTA,LEDF2DN);
+								break;*/
+							default:
+								CLR_BITS(PORTA,LEDF2UP|LEDF2DN);
+								break;
+						}
+						break;
+					case FLOOR_NUM_3:
+						SPIputc(NUM3);
+						switch(rxBuffer[2])
+						{/*
+							case CALL_BTN_UP:
+								CLR_BITS(PORTA,LEDF3UP);
+								break;
+							case CALL_BTN_DN:
+								CLR_BITS(PORTA,LEDF3DN);
+								break;*/
+							default:
+								CLR_BITS(PORTA,LEDF3UP|LEDF3DN);
+								break;
+						}
+						break;
+				}
+			}
+			else if(rxBuffer[0] == 0xEE)
+				SPIputc(NUME);
+			
+			canRXFlag = 0;
+		}
+	
+		if (PORTB & BUTF1UP)
+		{
+			PORTA |= LEDF1UP;
+			txBuffer[0] = CALL_BTN_PRESS;
+			txBuffer[1] = FLOOR_NUM_1;
+			txBuffer[2] = CALL_BTN_UP;
+			sendCanFrame(txMessage);
+		}
+		if (PORTB & BUTF1DN)
+		{
+			PORTA |= LEDF1DN;
+			txBuffer[0] = CALL_BTN_PRESS;
+			txBuffer[1] = FLOOR_NUM_1;
+			txBuffer[2] = CALL_BTN_DN;
+			sendCanFrame(txMessage);
+		}
+		if (PORTB & BUTF2UP)
+		{
+			PORTA |= LEDF2UP;
+			txBuffer[0] = CALL_BTN_PRESS;
+			txBuffer[1] = FLOOR_NUM_2;
+			txBuffer[2] = CALL_BTN_UP;
+			sendCanFrame(txMessage);
+		}
+		if (PORTB & BUTF2DN)
+		{
+			PORTA |= LEDF2DN;
+			txBuffer[0] = CALL_BTN_PRESS;
+			txBuffer[1] = FLOOR_NUM_2;
+			txBuffer[2] = CALL_BTN_DN;
+			sendCanFrame(txMessage);
+		}
+		if (PORTB & BUTF3UP)
+		{
+			PORTA |= LEDF3UP;
+			txBuffer[0] = CALL_BTN_PRESS;
+			txBuffer[1] = FLOOR_NUM_3;
+			txBuffer[2] = CALL_BTN_UP;
+			sendCanFrame(txMessage);
+		}
+		if (PORTB & BUTF3DN)
+		{
+			PORTA |= LEDF3DN;
+			txBuffer[0] = CALL_BTN_PRESS;
+			txBuffer[1] = FLOOR_NUM_3;
+			txBuffer[2] = CALL_BTN_DN;
+			sendCanFrame(txMessage);
+		}
+	}
+}
