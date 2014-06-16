e37af31751fa02e42b1cd9b3965e0fdac4bbcd3d
Alan Cranbury <alancranbury@gmail.com>
2014-06-16 15:02:10 -0400

    Updated Server.py

--- DataLogger/server.py
+++ DataLogger/server.py
@@ -1,51 +1,21 @@
 #!/usr/bin/python           # This is server.py file
 
-import socket
-import sys
-from thread import *
+import SocketServer
+from threading import Thread
 
-HOST = ''                   # Symbolic name meaning all available interfaces
-PORT = 8888                 # Arbitrary non-privilieged port
+class service(SocketServer.BaseRequestHandler):
+    def handle(self):
+        data = 'dummy'
+        print "Client connected with ", self.client_address
+        while len(data):
+            data = self.request.recv(1024)
+            self.request.send(data)
+            
+        print "Client Exited"
+        self.request.close()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
 
-try:
-    s.bind((HOST, PORT))
-except socket.error, msg:
-    print 'Bind failed. Error Code: ' + str(msg[0]) + ' Message: ' + msg[1]
-    sys.exit()
-
-print 'Socket bind complete'
-
-s.listen(10)
-print 'Socket now listening'
-
-# Function for handling connections. This will be used to create threads
-def clientThread(conn):
-    # Sending message to connected client
-    conn.send('Welcome to the server. Type something and hit enter\n')
-
-    # Infinite loope so that function does not terminate and thread does not end
-    while True:
-        # Receiving from client
-        data = conn.recv(1024)
-        reply = 'OK...' + data
-        if not data:
-            break
-
-        conn.sendall(reply)
-
-    # Came out of loop
-    conn.close()
-
-# Now keep talking with the client
-while 1:
-    # Wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-
-    # Start a new thread (function, (args))
-    start_new_thread(clientThread, (conn,))
-
-s.close()
+server = ThreadedTCPServer(('',1520), service)
+server.serve_forever()
